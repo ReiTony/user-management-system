7f8cfd4bfa9a4ad18836c6152bd4154970ed1c1f
Miguel Carino <antoncarino.work@gmail.com>
2024-12-25 20:27:28 +0800

    fix: update dotenv config path for test environment

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './.env.test' });
+require('dotenv').config({ path: './test/.env.test' });
 module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'json', 'ts'],
